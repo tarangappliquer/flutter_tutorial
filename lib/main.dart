7cbace2a6740d53329264af264bc7728eeacdf7e
Tarang Kalariya <tarang.appliquer@gmail.com>
2026-02-11 11:21:48 +0530

    resolution bug resolved for riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_tutorial/src/app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(ProviderScope(child: const App()));
 }
